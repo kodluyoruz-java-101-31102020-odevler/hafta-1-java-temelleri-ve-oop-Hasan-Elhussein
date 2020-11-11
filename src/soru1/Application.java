5dd096ae141cdcbc8ab6128a2f2785f7ed24073f
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2020-11-11 15:16:44 +0300

    soru1 added.

---
+++ src/soru1/Application.java
@@ -0,0 +1,22 @@
+package soru1;
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String password = "123aBc";
+		
+		System.out.println("Lütfen şifreyi giriniz: ");
+		String input = scan.next();
+		
+		
+		if(input.equals(password))
+			System.out.println("Giriş Başarılı!");
+		else
+			System.out.println("Giriş Başarısız");
+		
+	}
+
+}
